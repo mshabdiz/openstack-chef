80e10bf3dfa17a2cc9d683ec25ec6b18a7b531a5
Nachi Ueno <nachi@nttmcl.com>
2012-10-09 15:30:05 -0700

    Fixed design

--- cookbooks/freecloud/recipes/default.rb
+++ cookbooks/freecloud/recipes/default.rb
@@ -1,20 +1,8 @@
 # Turns out that AppArmor isn't good for your health...
-package "apparmor" do
-    action :remove
-end
-
-cookbook_file "/etc/apt/sources.list" do
-    source "etc/apt/sources.list"
-    owner "root"
-    group "root"
-    mode 0644
-end
 
 execute "sudo dd if=/dev/zero of=/opt/openstack/swap bs=1G count=50;sudo mkswap /opt/openstack/swap;sudo swapon /opt/openstack/swap" do
     not_if "swapon -s | grep /opt/openstack/swap"
 end
-
-execute "sudo add-apt-repository ppa:openstack-release/2011.3"
 execute "sudo ifconfig eth1 up"
 
 execute "sudo ufw allow from 10.0.100.0/24 to any port 22"
@@ -24,6 +12,15 @@ execute "sudo ufw enable"
 execute "apt-get update" do
     command "sudo apt-get update"
     action :run
+end
+
+directory "/opt/stack/.ssh" do
+    recursive true
+end
+
+user "stack" do
+   home "/opt/stack"
+   shell "/bin/bash"
 end
 
 template "/opt/stack/.ssh/authorized_keys" do
